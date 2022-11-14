3639f24e022ad8895f2a20b2cc7d77bd5808930a
Andrew Novoselac <andrew.novoselac@shopify.com>
2022-11-14 14:29:30 -0500

    First commit

---
+++ test/speculos_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class SpeculosTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Speculos::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
